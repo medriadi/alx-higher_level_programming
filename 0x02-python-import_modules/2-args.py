2209794e27fd2a7551a764378b63699ba077c6de
medriadi <mohamedriadi@outlook.com>
2024-01-04 17:18:47 +0100

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if (__name__ == "__main__"):
+    import sys
+    argv = sys.argv
+    argc = (len(sys.argv)) - 1
+    if (argc == 0):
+        print("0 arguments.")
+    elif (argc == 1):
+        print("1 argument:\n1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
